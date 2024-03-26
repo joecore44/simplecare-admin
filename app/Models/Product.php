e27e1fdeb76b7d5d4e8e9be105a831966ed4f3b1
Joe Shepard <shepard.joseph.m@gmail.com>
2024-03-25 19:10:44 -0600

    Add media library functionality to Product model and create media table migration

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,11 +5,16 @@ namespace App\Models;
 use Cknow\Money\Rules\Money;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\Conversions\Manipulations;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 use Staudenmeir\LaravelAdjacencyList\Eloquent\HasRecursiveRelationships;
 
-class Product extends Model
+class Product extends Model implements HasMedia
 {
     use HasFactory, HasRecursiveRelationships;
+    use InteractsWithMedia;
 
     public function formattedPrice()
     {
@@ -19,5 +24,11 @@ class Product extends Model
     public function variations()
     {
         return $this->hasMany(Variation::class);
+    }
+
+    public function registerMediaConversions(?Media $media = null): void
+    {
+        $this->addMediaConversion('thumb200x200')
+        ->fit(Manipulations::CROP, 200, 200);
     }
 }
